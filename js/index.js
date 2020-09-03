79399917b1e993b880a55597942c7471b22292e2
Alan Silvester <alan.twister5@gmail.com>
2020-09-03 20:25:44 -0300

    Cuarto commit

--- js/index.js
+++ js/index.js
@@ -1,29 +1,32 @@
-var btnCataratas = document.getElementById("btnCataratas");
-var btnBariloche = document.getElementById("btnBariloche");
-var btnGrutas = document.getElementById("btnGrutas");
-var btnBolson = document.getElementById("btnBolson");
-var btnCordoba = document.getElementById("btnCordoba");
-var btnRosario = document.getElementById("btnRosario");
-var btnUshuaia = document.getElementById("btnUshuaia");
-var btnParana = document.getElementById("btnParana");
-var btnNinguno = document.getElementById("btnNinguno");
-
 var intentos = 4;
+var solucion = "Bariloche";
+var idResolver = document.getElementById("idResolver");
+var cantIntento = 4;
 
-var btnAyuda1 = document.getElementById("btnAyuda1");
-var btnAyuda2 = document.getElementById("btnAyuda2");
+idResolver.addEventListener("click", resolver);
 
-btnAyuda1.addEventListener("click", ayuda1);
-btnAyuda2.addEventListener("click", ayuda2);
-
-
-
-function ayuda1() {
-  swal("Ciudad de los egresados.");
+function resolver() {
+  let nombre = prompt("Ingrese su nombre");
+  let cantIntento = 4;
+  let respuesta = prompt("Hola " + nombre + " ingresa tu respuesta!");
+  for (let index = 0; index < intentos; index++) {
+    cantIntento--;    
+    if (respuesta == solucion) {
+      swal("Felicitaciones " + nombre + " la respuesta es correcta!!");
+      break;
+    } else if (solucion != respuesta && cantIntento == 3) {
+      alert("Respuesta incorrecta!! Le quedan "+ cantIntento + " intentos!!");
+      respuesta = prompt("Ingrese otra respuesta!");
+    } else if (solucion != respuesta && cantIntento == 2) {
+        alert("Respuesta incorrecta!! Le quedan "+ cantIntento + " intentos!!");
+        alert("Una ayuda: es la cuidad de los Egresados!!")
+        respuesta = prompt("Ingrese otra respuesta!");
+    } else if (solucion != respuesta && cantIntento == 1) {
+        alert("Respuesta incorrecta!! Le quedan "+ cantIntento + " intentos!!");
+        alert("Última ayuda: en ella se encuentra el Parque Nacional Nahuel Huapi")
+        respuesta = prompt("Ingrese otra respuesta!");
+    } else if (solucion != respuesta && cantIntento == 0) {
+        swal("Error la respuesta es " + solucion + "!!");
+    }
+  }
 }
-
-function ayuda2() {
-  swal("Se encuentra el Parque Nacional Nahuel Huapi.");
-}
-
-
